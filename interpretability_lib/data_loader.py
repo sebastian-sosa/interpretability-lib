1f14adda8459cf0afe9ab55eeb4613380a82eff8
Sebastian Sosa <ssosarippe@gmail.com>
2023-11-16 17:22:14 -0300

    Add caltech101 dataloader

--- interpretability_lib/data_loader.py
+++ interpretability_lib/data_loader.py
@@ -1,3 +1,8 @@
+import os
+import tarfile
+import urllib.request
+import zipfile
+
 import torch
 from torchvision import datasets, transforms
 
@@ -31,3 +36,47 @@ def load_mnist(batch_size=32, num_workers=0):
     test_loader = torch.utils.data.DataLoader(test_set, batch_size=batch_size, shuffle=False, num_workers=num_workers)
 
     return train_loader, test_loader
+
+
+def load_caltech101(data_dir='./data', batch_size=32, num_workers=0):
+    """Load the Caltech-101 dataset."""
+    # Define the URL and filename of the dataset
+    CALTECH_101_URL = 'https://data.caltech.edu/records/mzrjq-6wc02/files/caltech-101.zip'
+    filename = CALTECH_101_URL.split('/')[-1]
+
+    # Download the dataset if it doesn't exist
+    if not os.path.exists(filename):
+        print('Downloading dataset...')
+        urllib.request.urlretrieve(CALTECH_101_URL, filename)
+
+    # Extract the zip file if it hasn't been extracted
+    if not os.path.exists(os.path.join(data_dir, 'caltech-101')):
+        print('Extracting zip file...')
+        with zipfile.ZipFile(filename, 'r') as zip_ref:
+            zip_ref.extractall(data_dir)
+
+    # Extract the tar.gz file if it hasn't been extracted
+    if not os.path.exists(os.path.join(data_dir, 'caltech-101', '101_ObjectCategories')):
+        print('Extracting tar.gz file...')
+        with tarfile.open(os.path.join(data_dir, 'caltech-101', '101_ObjectCategories.tar.gz'), 'r:gz') as tar:
+            tar.extractall(os.path.join(data_dir, 'caltech-101'))
+
+    # Define the transformations
+    transform = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+
+    # Load the dataset
+    dataset_dir = os.path.join(data_dir, 'caltech-101', '101_ObjectCategories')
+    dataset = datasets.ImageFolder(dataset_dir, transform=transform)
+    dataloader = torch.utils.data.DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=num_workers
+    )
+
+    return dataloader
