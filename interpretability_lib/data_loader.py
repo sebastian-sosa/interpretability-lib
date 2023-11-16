83b144baec49c56e7faa93d3e75c5ccbee82675c
Sebastian Sosa <ssosarippe@gmail.com>
2023-11-16 15:59:41 -0300

    Add model and dataset loaders

--- interpretability_lib/data_loader.py
+++ interpretability_lib/data_loader.py
@@ -1,2 +1,33 @@
-def load_data():
-    return "hello"
+import torch
+from torchvision import datasets, transforms
+
+
+def load_cifar10(batch_size=32, num_workers=0):
+    """Load the CIFAR-10 dataset."""
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+
+    train_set = datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+    train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+
+    test_set = datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+    test_loader = torch.utils.data.DataLoader(test_set, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+
+    return train_loader, test_loader
+
+def load_mnist(batch_size=32, num_workers=0):
+    """Load the MNIST dataset."""
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.1307,), (0.3081,))
+    ])
+
+    train_set = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
+    train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+
+    test_set = datasets.MNIST(root='./data', train=False, download=True, transform=transform)
+    test_loader = torch.utils.data.DataLoader(test_set, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+
+    return train_loader, test_loader
