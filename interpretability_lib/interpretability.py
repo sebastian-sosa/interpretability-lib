56befc8e1c2465fd73a7849ba83c54d1c7b9f940
Sebastian Sosa <ssosarippe@gmail.com>
2023-11-17 17:56:06 -0300

    Add example notebook that shows usage, improvements to compute_saliency_map

--- interpretability_lib/interpretability.py
+++ interpretability_lib/interpretability.py
@@ -1,20 +1,25 @@
 import torch
+from torch.autograd import Variable
 
 
-def compute_saliency_map(model: torch.nn.Module, inputs: torch.Tensor) -> torch.Tensor:
+def compute_saliency_map(input_image: torch.Tensor, model: torch.nn.Module) -> torch.Tensor:
     """
     Compute the saliency map of a given model for a given input.
 
     Args:
+        input_image (torch.Tensor): The input image.
         model (torch.nn.Module): The model to compute the saliency map for.
-        inputs (torch.Tensor): The inputs to the model.
 
     Returns:
         torch.Tensor: The computed saliency map.
     """
-    model.eval()
-    inputs.requires_grad_()
-    outputs = model(inputs)
-    outputs.backward(torch.ones_like(outputs))
-    saliency_map = inputs.grad.data
+    input_image = Variable(input_image, requires_grad=True)
+    output = model(input_image)
+    output_idx = output.argmax()
+    output_max = output[0, output_idx]
+    output_max.backward()
+    saliency_map = input_image.grad.data
+    saliency_map = saliency_map.abs()
+    saliency_map, _ = torch.max(saliency_map, dim=1)
+    saliency_map = saliency_map.squeeze()
     return saliency_map
