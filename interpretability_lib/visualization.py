48ed4a3f2770f9c93b05db304551817a1ffa89b7
Sebastian Sosa <ssosarippe@gmail.com>
2023-11-17 17:54:22 -0300

    Simplify visualization module, remove unused data loader module

--- interpretability_lib/visualization.py
+++ interpretability_lib/visualization.py
@@ -1,64 +1,35 @@
-from typing import List
-
 import matplotlib.pyplot as plt
 import numpy as np
+from PIL import Image
 
 
-class VisualizationModule:
-    def display_heatmap(self, heatmap: np.ndarray) -> None:
-        """
-        Display a heatmap.
+def display_saliency_map(image: Image.Image, heatmap: np.array) -> None:
+    """
+    Display a heatmap.
 
-        Args:
-            heatmap (np.ndarray): The heatmap to display. Expected shape is (height, width).
-        """
-        plt.imshow(heatmap, cmap='hot', interpolation='nearest')
-        plt.show()
+    Args:
+        image (np.ndarray): Original image on which to overlay a heatmap.
+        heatmap (np.ndarray): The heatmap to display.
+    """
+    plt.figure(figsize=(15, 15))
 
-    def display_overlay(self, image: np.ndarray, heatmap: np.ndarray, agg_func=np.amax) -> None:
-        """
-        Display a heatmap overlayed on an image.
+    # Display the cropped image
+    plt.subplot(1, 3, 1)
+    plt.imshow(image)
+    plt.axis('off')
+    plt.title('Cropped Image')
 
-        Args:
-            image (np.ndarray): The image to display. Expected shape is (height, width, channels).
-            heatmap (np.ndarray): The heatmap to overlay. 
-                Expected shape is (height, width, channels) or (height, width).
-            agg_func (function, optional): The aggregation function to apply across the channel
-                dimension of the heatmap if it has 3 dimensions. Defaults to np.amax.
-        """
-        # If the heatmap has 3 dimensions, aggregate across the channel dimension
-        # if len(heatmap.shape) == 3:
-        #     heatmap = agg_func(heatmap, axis=0)
-        if len(heatmap.shape) == 2:
-            heatmap = heatmap[None, :, :]  # Add a singleton dimension
+    # Display the saliency map
+    plt.subplot(1, 3, 2)
+    plt.imshow(heatmap, cmap=plt.cm.hot)
+    plt.axis('off')
+    plt.title('Saliency Map')
 
-        plt.imshow(image)
-        plt.imshow(heatmap, cmap='hot', interpolation='nearest', alpha=0.5)
-        plt.show()
+    # Display the saliency map applied on top of the cropped image
+    plt.subplot(1, 3, 3)
+    plt.imshow(image, alpha=0.5)
+    plt.imshow(heatmap, cmap=plt.cm.hot, alpha=0.5)
+    plt.axis('off')
+    plt.title('Saliency Map on Image')
 
-    def display_image(self, image: np.ndarray) -> None:
-        """
-        Display an image.
-
-        Args:
-            image (np.ndarray): The image to display. Expected shape is (height, width, channels).
-        """
-        plt.imshow(image)
-        plt.show()
-
-    def display_grid(self, images: List[np.ndarray], titles: List[str]) -> None:
-        """
-        Display a grid of images.
-
-        Args:
-            images (List[np.ndarray]): The images to display. 
-                Each image should have shape (height, width, channels).
-            titles (List[str]): The titles for the images.
-        """
-        n = len(images)
-        fig, axs = plt.subplots(1, n, figsize=(n * 5, 5))
-        for i, (img, title) in enumerate(zip(images, titles)):
-            axs[i].imshow(img)
-            axs[i].set_title(title)
-            axs[i].axis('off')
-        plt.show()
+    plt.show()
