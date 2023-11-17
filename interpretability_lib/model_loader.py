f85792b7fe208e205a68b67b06a5d6797e46a241
Sebastian Sosa <ssosarippe@gmail.com>
2023-11-17 17:55:26 -0300

    Use  instead of  parameters when loading torchvision models

--- interpretability_lib/model_loader.py
+++ interpretability_lib/model_loader.py
@@ -1,9 +1,16 @@
-import torch
 from torchvision import models
 
 
 def load_resnet50(pretrained=True):
     """Load a pretrained ResNet-50 model."""
-    model = models.resnet50(pretrained=pretrained)
+    model = models.resnet50(weights='IMAGENET1K_V1')
     model.eval()
     return model
+
+
+def load_vgg16(pretrained=True):
+    """Load a pretrained VGG16 model."""
+    model = models.vgg16(weights='IMAGENET1K_V1')
+    model.eval()
+    return model
+
